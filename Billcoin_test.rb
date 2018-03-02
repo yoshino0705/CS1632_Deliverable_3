5c5f8a36aad1777993a778e0a3e8e91454634b24
Yoshino <weihaomouse@gmail.com>
2018-03-02 11:25:17 -0500

    added more tests that should pass

--- Billcoin_test.rb
+++ Billcoin_test.rb
@@ -28,27 +28,95 @@ class Billcoin_test < Minitest::Test
 
 	# UNIT TESTS FOR METHOD parse_info()
 
-	def test_parse_info
-
+	def test_parse_info_block_number
+		info = @bc.parse_info '123456|test_prev_hash|test_transaction|test_timestamp|test_hash'
+		assert_equal info['id'], 123456
 	end
 
-	# UNIT TESTS FOR METHOD get_string_to_hash()
+	def test_parse_info_prev_hash
+		info = @bc.parse_info 'test_block_num|8d96|test_transaction|test_timestamp|test_hash'
+		assert_equal info['prev_hash'], '8d96'
+	end
+
+
+	def test_parse_info_transactions
+		info = @bc.parse_info 'test_block_num|test_prev_hash|tt1:tt2:tt3|test_timestamp|test_hash'
+		assert_equal info['transaction'].length, 3
+	end
+
+
+	def test_parse_info_timestamp
+		info = @bc.parse_info 'test_block_num|test_prev_hash|test_transaction|15000000000.6598754|test_hash'
+		assert_equal info['ts'][:sec], 15000000000
+		assert_equal info['ts'][:nsec], 6598754
+	end
+
+	def test_parse_info_hash
+		info = @bc.parse_info 'test_block_num|test_prev_hash|test_transaction|test_timestamp|fd15'
+		assert_equal info['self_hash'], 'fd15'
+	end
+
+	def test_parse_info_original
+		info = @bc.parse_info 'test_block_num|test_prev_hash|test_transaction|test_timestamp|test_hash'
+		assert_equal info['original'], 'test_block_num|test_prev_hash|test_transaction|test_timestamp|test_hash'
+	end
+
+
+	# UNIT TEST FOR METHOD get_string_to_hash()
 
 	def test_get_string_to_hash
-
+		info = @bc.parse_info '0|test_prev_hash|test_transaction|0.0|test_hash'
+		assert_equal @bc.get_string_to_hash(info), '0|test_prev_hash|test_transaction|0.0'
 	end
+
 
 	# UNIT TESTS FOR METHOD validate_address()
 
-	def test_validate_address
-
+	def test_validate_address_valid
+		address = 'abc'
+		s, e = @bc.validate_address address
+		assert_equal s, true
+		assert_equal e, ''
 	end
 
-	# UNIT TESTS FOR METHOD update_billcoins()
-
-	def test_update_billcoins
-
+	def test_validate_address_not_alphabetic
+		address = 'ab666'
+		s, e = @bc.validate_address address
+		assert_equal s, false
+		assert_equal e, "The address #{address} is not alphabetic."
 	end
+
+	def test_validate_address_too_long
+		address = 'abccccccccc'
+		s, e = @bc.validate_address address
+		assert_equal s, false
+		assert_equal e, "Invalid address length for #{address}, should be at most 6 alphabetic characters."
+	end
+
+
+	# UNIT TESTS FOR METHOD validate_billcoins()
+
+	def test_validate_billcoins_normal
+		@bc.billcoins['user1'] = 500
+		s, e = @bc.validate_billcoins 'user1'
+		assert_equal s, true
+		assert_equal e, ''
+	end
+
+	def test_validate_billcoins_no_such_address
+		@bc.billcoins['user1'] = 500
+		s, e = @bc.validate_billcoins 'user2'
+		assert_equal s, false
+		assert_equal e, "Internal Error: user2 does not exist in the billcoin dictionary"
+	end
+
+	def test_validate_billcoins_negative
+		@bc.billcoins['user1'] = -500
+		s, e = @bc.validate_billcoins 'user1'
+		assert_equal s, false
+		assert_equal e, "Invalid block, address user1 has -500 billcoins!"
+	end
+
 
 	# UNIT TESTS FOR METHOD validate_timestamps()
 
@@ -56,11 +124,13 @@ class Billcoin_test < Minitest::Test
 
 	end
 
+
 	# UNIT TESTS FOR METHOD validate_hash()
 
 	def test_validate_hash
 
 	end
+
 
 	# UNIT TESTS FOR METHOD validate_first_block()
 
@@ -68,21 +138,10 @@ class Billcoin_test < Minitest::Test
 
 	end
 
+
 	# UNIT TESTS FOR METHOD validate_block()
 
 	def test_validate_block
-
-	end
-
-	# UNIT TESTS FOR METHOD print_billcoins()
-
-	def test_print_billcoins
-
-	end
-
-	# UNIT TESTS FOR METHOD validate_block_chain()
-
-	def test_validate_block_chain
 
 	end
 
